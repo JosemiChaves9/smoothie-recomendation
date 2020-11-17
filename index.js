753497d8fb79f2ed07577a78c0d4f47e4347fca7
Albert Vazquez <hola@albertvazquez.es>
2020-11-17 11:27:25 +0100

    split code into functions

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-let fruits = ['🍎', '🍐', '🍑', '🍌', '🍉', '🍇', '🍓', '🥥', '🍈', '🥝', '🍍'];
-let smoothie = [];
+const fruits = ['🍎', '🍐', '🍑', '🍌', '🍉', '🍇', '🍓', '🥥', '🍈', '🥝', '🍍'];
 
+const generateRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * max) - min;
+}
+
+const generateRandomFruit = () => {
+    const randomNum = generateRandomNumber(0, fruits.length);
+    return fruits[randomNum];
+}
+
+const generateUniqueFruitForSmoothie = (smoothie) => {
+    const randomFruit = generateRandomFruit();
+    if (smoothie.includes(randomFruit)) {
+        return generateUniqueFruitForSmoothie(smoothie);
+    } else {
+        return randomFruit;
+    }
+}
+
+const generateSmoothie = (numberOfFruits) => {
+    const smoothie = [];
+    while (smoothie.length < numberOfFruits) {
+        const randomFruit = generateUniqueFruitForSmoothie(smoothie);
+        smoothie.push(randomFruit);
+    };
+    return smoothie;
+}
+
+const result = generateSmoothie(4);
+
+console.log(result);
+/*
 while (smoothie.length < 4) {
     for (let i = 0; i < 4; i++) {
-        let randomNum = Math.floor(Math.random() * 10);
+        let randomNum =
         if (smoothie.includes(fruits[randomNum])) {
             smoothie.pop();
         } else if (smoothie.length === 4) { ///because sometimes the array is 6 items long
@@ -15,4 +45,4 @@ while (smoothie.length < 4) {
 };
 
 
-console.log(smoothie);
+console.log(smoothie);*/
